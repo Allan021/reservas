f2eaefe02ab87702c3a2424dedb27477ade4d9e0
allan021 <allan.aceituno20@gmail.com>
2025-04-04 13:46:50 -0600

    fix issues

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -6,17 +6,21 @@ interface ModalProps {
 }
 
 const Modal: FC<ModalProps> = ({ isOpen, onClose, children }) => {
-  const modalRef = useRef(null);
+  const modalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const handleKeyDown = (event) => {
+    const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "Escape") {
         onClose();
       }
     };
 
-    const handleClickOutside = (event) => {
-      if (modalRef.current && !modalRef.current.contains(event.target)) {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (!modalRef.current) {
+        return;
+      }
+
+      if (modalRef.current.contains(event.target as Node)) {
         onClose();
       }
     };
@@ -36,34 +40,33 @@ const Modal: FC<ModalProps> = ({ isOpen, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-100/90 backdrop-blur-sm transition-opacity duration-300 ease-in-out opacity-100 px-4">
-    <div
-      ref={modalRef}
-      className="relative w-full max-w-md p-6 md:p-8 bg-white rounded-2xl shadow-2xl max-h-[90vh] overflow-y-auto max-w-[328px]"
-    >
-      <button
-        onClick={onClose}
-        className="absolute top-4 right-4 text-gray-600 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300 rounded-full"
-        aria-label="Cerrar modal"
+      <div
+        ref={modalRef}
+        className="relative w-full max-w-md p-6 md:p-8 bg-white rounded-2xl shadow-2xl max-h-[90vh] overflow-y-auto max-w-[328px]"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-gray-600 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300 rounded-full"
+          aria-label="Cerrar modal"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
-      {children}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        {children}
+      </div>
     </div>
-  </div>
-  
   );
 };
 
